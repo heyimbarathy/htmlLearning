90445122818082a1d35c753bcace9cd25766e8b1
Bharathi <heyimbarathy@gmail.com>
2021-05-29 09:00:46 +0530

    Basic form validation with CSS style properties

--- FormValidation2/script.js
+++ FormValidation2/script.js
@@ -1,8 +1,24 @@
 function validate() {
     var username = document.getElementById('uname');
     var password = document.getElementById('pass');
-    if (username.value.trim() === "" || password.value.trim() === "") {
-        alert('No blank values are allowed');
+    if (username.value.trim() === "") {
+        //alert('Blank Username');
+        username.style.border = "solid 3px red";
+        document.getElementById('lbluser').style.visibility="visible";
+        return false;
+    }
+    else if (password.value.trim() === "") {
+        // alert('Blank Password');
+        password.style.border = "solid 3px red";
+        document.getElementById('lblpass').style.visibility="visible";
+        return false;
+    }
+    else if (password.value.trim().length < 5) {
+        alert('password too short');
+        return false;
+    }
+    else if (password.value.trim().length > 20) {
+        alert('Password too long');
         return false;
     }
     else{
